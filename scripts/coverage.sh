10591504dfe9be42088b9c5398ba9af6cc5e724c
YangHeeChang <diogmlckd@naver.com>
2025-06-05 18:30:35 +0900

    docker에서 run 할 때 옵션으로 host의 ip와 port 전달 시 coverage시 출력

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,11 +2,14 @@ if [ "$VIRTUAL_ENV" == "" ] && [ "$PYPDFFORM_ENV" != "container" ]; then
   source "./venv/bin/activate"
 fi
 
+HOST_IP=${HOST_IP:-localhost}
+HOST_PORT=${HOST_PORT:-8000}
+
 rm -rf htmlcov/
 coverage run -m pytest && coverage html
 
 if [ "$PYPDFFORM_ENV" == "container" ]; then
-  echo "Coverage report: http://localhost:8000/htmlcov/index.html"
+  echo "Coverage report: http://$HOST_IP:$HOST_PORT/htmlcov/index.html"
   bash ./scripts/http_server.sh > /dev/null 2>&1
 else
   google-chrome htmlcov/index.html
